596376eb35ff40c8a0802b5c44c4f36ff34f5dc0
ewarchul <ewarchul@gmail.com>
2021-04-20 22:13:46 +0200

    Change compilations flags

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define REP 1
+#define REP 50000
 
 CecData cd = {
   .prevDimension = 0,
@@ -13,11 +13,11 @@ CecData cd = {
 };
 
 int main() {
-  int year = 2015;
+  int year = 2017;
   char dataPath[50];
   sprintf(dataPath, "../data/cec%d", year);
-  int fn_max = 15;
-  int dims[1] = {10};
+  int fn_max = 30;
+  int dims[1] = {100};
   for (int d = 0; d < 1; ++d) {
     for (int fn = 1; fn < fn_max + 1; ++fn) {
       for (int i = 0; i < REP; i++) {
@@ -26,7 +26,7 @@ int main() {
         for (int i = 0; i < dims[d]; ++i) {
           input[i] = 0.1;
         }
-        cec2015_interface(dataPath, input, output, dims[d], 1, fn);
+        cec2017_interface(dataPath, input, output, dims[d], 1, fn);
        // printf("<f=%d|d=%d\toutput[0] = %0.3f\n",fn, dims[d], output[0]);
         free(output);
         free(input);
