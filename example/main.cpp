8ce0eb495f342469f5f975aa4d1477029549f5df
ewarchul <ewarchul@gmail.com>
2024-06-29 19:04:01 +0200

    Rename example dir

--- example/main.cpp
+++ example/main.cpp
@@ -8,35 +8,43 @@
 
 #include "cecxx/benchmark/evaluator.hpp"
 
-constexpr auto DIMENSION{10};
-constexpr auto CEC_STORAGE{"/home/ewarchul/open-source/optim/cec/data"};
+constexpr auto DIMENSION{10u};
+constexpr auto CEC_STORAGE{"/home/ewarchul/open-source/optim/cecxx/data"};
 
 namespace rv = ranges::views;
+using namespace cecxx;
 
 auto main() -> int {
   try {
     // Create an evaluator object for the CEC2017 benchmark
-    auto cec2017_eval = cecxx::benchmark::evaluator(cecxx::benchmark::cec_edition_t::cec2017, DIMENSION, CEC_STORAGE);
+    auto cec2017_eval = benchmark::evaluator(
+        cecxx::benchmark::cec_edition_t::cec2017, DIMENSION, CEC_STORAGE);
 
     // Prepare input which resembles multidimensional array
-    const auto input = std::vector<std::vector<double>>{rv::repeat(10.0) | rv::take(DIMENSION) | ranges::to_vector};
+    const auto input = std::vector<std::vector<f64>>{
+        rv::repeat(10.0) | rv::take(DIMENSION) | ranges::to_vector};
+
+
 
     // Evaluate given input on each optimization problem from CEC2017
     const auto start = std::chrono::system_clock::now();
-    for (const auto& fn : rv::closed_iota(1, 30)) {
+    for (const auto &fn : rv::closed_iota(11, 11)) {
       auto output = cec2017_eval(fn, input);
       fmt::println("fn = {}, output = {:}", fn, output);
     }
     fmt::println("Elapsed time: {}",
-                 std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start));
+                 std::chrono::duration_cast<std::chrono::microseconds>(
+                     std::chrono::system_clock::now() - start));
 
     return EXIT_SUCCESS;
     // Create a closure for 1st optimizaiton problem from CEC2017
-    const auto first_fn = [&eval = cec2017_eval](const auto& xs) { return eval(11, xs); };
+    const auto first_fn = [&eval = cec2017_eval](const auto &xs) {
+      return eval(11, xs);
+    };
     auto output = first_fn(input);
     //   fmt::println("fn = 11, output = {}", output);
 
-  } catch (std::exception& e) {
+  } catch (std::exception &e) {
     fmt::println("Error: {}", e.what());
   }
 
